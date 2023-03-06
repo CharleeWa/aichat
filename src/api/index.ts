f370f0b38809508eca0aac88fd96b841803c80c8
CharleeWa <18888351756@163.com>
2023-03-06 23:04:30 +0800

    chore: 切换为GPT-3.5模型

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,11 +2,7 @@ import request from '@/utils/request'
 
 export async function generate(msg): Promise<any> {
   const playload = {
-    model: 'text-davinci-003',
-    prompt: msg,
-    temperature: 0.3,
-    max_tokens: 2000,
-    stream: false,
+    messages: msg,
   }
-  return request.post('/aiapi/trans/v1/completions', playload)
+  return request.post('/', playload)
 }
--- src/views/indexcopy.vue
+++ src/views/indexcopy.vue
@@ -5,13 +5,8 @@ const list = ref([])
 const loading = ref(false)
 const finished = ref(false)
 const messae = ref('')
-const context = ref([
-  {
-    role: 'system',
-    content: '你是很有帮助的客服。',
-  },
-])
-// const price = ref(0)
+const context = ref('')
+const price = ref(0)
 const finisText = ref('没有更多消息')
 
 const onLoad = () => {
@@ -21,22 +16,35 @@ const onLoad = () => {
 }
 
 const generateDesc = async (content: any) => {
-  context.value.push({
-    role: 'user',
-    content,
-  })
-
   finisText.value = '此时一位工作人员正在疯狂码字...'
-  const res = await generate(context.value)
+  const res = await generate(content)
   finisText.value = '没有更多消息'
+  let resu = ''
+  const arr = ['？', '，', '；', '！', '?', ',', ';', '!', '。', '.']
+  if (arr.includes(res.data.choices[0].text.charAt(0)))
+    resu = res.data.choices[0].text.substr(1)
+  else
+    resu = res.data.choices[0].text
 
-  context.value.push({
-    role: 'assistant',
-    content: res,
-  })
+  if (resu.substring(0, 4) === '\n\n')
+    resu = resu.replace('\n\n', '')
+
+  if (resu.indexOf('↵↵') !== 0)
+    resu = resu.replace('↵↵', '')
+
+  const hasText = resu.trim()
+
+  // 将历史上下文传递给模型，生成响应
+  if (hasText)
+    context.value = `${context.value}\n${hasText}`
+
+  price.value += res.data.usage.total_tokens
+
+  // 如果要加下面这行代码，注意ai无内容返回时的判断逻辑
+  // <div class="token">本组对话消耗：￥${((res.data.usage.prompt_tokens + res.data.usage.completion_tokens) * 70 * 0.0000026).toFixed(5)}</div>
 
   list.value.push({
-    text: res,
+    text: hasText || '抱歉！突然不知道怎么回答你的问题了。',
     label: 'ai',
   })
 }
@@ -50,8 +58,13 @@ const addMessage = () => {
     label: 'you',
   })
 
+  if (context.value.length !== 0)
+    context.value = `${context.value}\n ${messae.value}`
+  else
+    context.value = `你是一名智能客服，用来回答用户提的问题。以下是提供的上下文，请分析后进行回答： \n ${messae.value}`
+
   // 触发openai回复
-  generateDesc(messae.value)
+  generateDesc(context.value)
 
   messae.value = ''
 }
